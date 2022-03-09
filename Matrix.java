dd3d0b369c64fec7ce371fcf38c9b441ad3c341a
D1935827V <dvestal2012@gmail.com>
2022-03-09 14:49:56 -0800

    data labs

---
+++ Matrix.java
@@ -0,0 +1,48 @@
+public class Matrix {
+    private final int[][] matrix;
+
+    // store matrix
+    public Matrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    // Hack: create toString method using nested for loops to format output of a matrix
+    public String toString(){//overriding the toString() method
+        String reverse_str = "";
+        String str = "";
+        for (int[] ints : matrix) {
+            for (int col = 0; col < ints.length; col++) {
+                reverse_str = ints[col] + reverse_str;
+                str = str + ints[col];
+            }
+            str = str + "\n";
+            reverse_str = "\n" + reverse_str;
+        }
+        str = str+reverse_str+"\n";
+        return str;
+    }
+
+    // declare and initialize a matrix for a keypad
+    static int[][] keypad() {
+        return new int[][]{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, {-1, 0, -1} };
+    }
+
+    // declare and initialize a random length arrays
+    static int[][] numbers() {
+        return new int[][]{ { 0, 1 },
+                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 } };
+    }
+
+    // tester method for matrix formatting
+    public static void main() {
+        Matrix m0 = new Matrix(keypad());
+        System.out.println("Keypad:");
+        System.out.println(m0);
+
+        Matrix m1 = new Matrix(numbers());
+        System.out.println("Numbers Systems:");
+        System.out.println(m1);
+    }
+
+}
